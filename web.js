60ffadbcb61963e0a3270be1f524b84d7215c375
jackgilbert <jack.gilbert.tgn@gmail.com>
2013-07-10 00:14:11 +0000

    copied working web.js

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('./index.html', function read(err, data) {
-if (err) throw err;
-console.log(data);
+app.get('/', function(request, response) {
+    var fs = require('fs');
+    var buf = new Buffer fs.readFileSync('index.html', 'utf-8');
+    response.send(buf.toString());
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
